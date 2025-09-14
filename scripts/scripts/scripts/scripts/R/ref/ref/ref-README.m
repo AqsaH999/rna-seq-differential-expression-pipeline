b418c2cbde9e16e800cbfcb34fe41771c914c392
Aqsa Hamdani <161809807+AqsaH999@users.noreply.github.com>
2025-09-14 16:50:32 +0400

    Create ref-README.m

---
+++ scripts/scripts/scripts/scripts/R/ref/ref/ref-README.m
@@ -0,0 +1,10 @@
+# Reference Genome and Annotation
+
+Download reference genome (FASTA) and GTF annotation files:
+
+## Example for human GRCh38:
+- Genome FASTA: ftp://ftp.ensembl.org/pub/release-109/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+- Annotation GTF: ftp://ftp.ensembl.org/pub/release-109/gtf/homo_sapiens/Homo_sapiens.GRCh38.109.gtf.gz
+
+Place genome in: `ref/genome.fa`
+Place annotation in: `ref/annotation.gtf`
